821b787089da279474c4ce3e0f385d7b8b9210ba
rajeshrc12 <rajesh.charhajari@gmail.com>
2025-05-04 13:36:42 +0530

    marketplace page added

--- app/marketplace/layout.tsx
+++ app/marketplace/layout.tsx
@@ -1,7 +1,7 @@
 import Sidebar from "@/components/sidebar";
 import React from "react";
 
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+const MarkeplaceLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-screen">
       <Sidebar />
@@ -10,4 +10,4 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-export default DashboardLayout;
+export default MarkeplaceLayout;
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,17 +2,32 @@ import { auth } from "@/auth";
 import Image from "next/image";
 import React from "react";
 import { Button } from "./ui/button";
+import { ShoppingBag, User } from "lucide-react";
+import ActiveLink from "./active-link";
 
 const Sidebar = async () => {
   const session = await auth();
+  const navItems = [
+    {
+      name: "Marketplace",
+      path: "/marketplace",
+      icon: <ShoppingBag className="h-4 w-4" />,
+    },
+    {
+      name: "Credentials",
+      path: "/marketplace/credential",
+      icon: <User className="h-4 w-4" />,
+    },
+  ];
   if (!session?.user?.image) return;
   return (
     <div className="w-[250px] h-full flex flex-col justify-between border-r px-2">
       <div className="flex flex-col gap-10">
         <div>Logo</div>
         <div className="flex flex-col gap-5">
-          <Button variant={"ghost"}>Marketplace</Button>
-          <Button variant={"ghost"}>Credential</Button>
+          {navItems.map((item) => (
+            <ActiveLink key={item.path} href={item.path} icon={item.icon} name={item.name} />
+          ))}
         </div>
       </div>
       <div className="p-2">
